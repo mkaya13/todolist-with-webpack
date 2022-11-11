1b1d5ac88b68d2aac3ccacafd17f5c2c607e761b
z4cope <63551733+z4cope@users.noreply.github.com>
2022-11-11 14:11:10 +0200

    Add edit, update and clear mocks

---
+++ src/__mocks__/edit-update-clear.js
@@ -0,0 +1,19 @@
+export const editTask = (oldTask, newTask) => {
+  const newTaskObj = {
+    description: newTask.description,
+    completed: false,
+    index: oldTask.index,
+  };
+  return newTaskObj;
+};
+export const updateCompleteStatus = (task) => {
+  task.completed = !task.completed;
+  return task;
+};
+export const clearAllCompleted = (taskList) => {
+  taskList = taskList.filter((task) => task.completed !== true);
+  taskList.forEach((taskList, no) => {
+    taskList.index = no + 1;
+  });
+  return taskList;
+};
